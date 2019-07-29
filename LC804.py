e3f3ad508d6efc90be2f2f033deeda57e1700ad0
JC <jjing.cc@gmail.com>
2019-07-29 14:58:22 +0800

    Update LC804.py

--- LC804.py
+++ LC804.py
@@ -1,3 +1,4 @@
+#slow and brute force one
 class Solution:
     def uniqueMorseRepresentations(self, words):
         mosi = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..",
@@ -9,3 +10,25 @@ class Solution:
                 X[n] = mosi[ord(i)-97]
             out.append("".join(X))
         return len(set(out))
+
+    
+    #a faster solution
+    class Solution:
+    def uniqueMorseRepresentations(self, words):
+        mosi = [".-","-...","-.-.","-..",".","..-.",
+                "--.","....","..",".---","-.-",".-..",
+                "--","-.","---",".--.","--.-",".-.",
+                "...","-","..-","...-",".--","-..-",
+                "-.--","--.."]
+        dic = {"a" : ".-", "b" : "-...", "c" : "-.-.", "d" : "-..", "e" : ".", "f" : "..-.", 
+               "g" : "--.", "h" : "....", "i" : "..", "j" : ".---", "k" : "-.-", "l" : ".-..", 
+               "m" : "--", "n" : "-.", "o" : "---", "p" : ".--.", "q" : "--.-", "r" : ".-.", 
+               "s" : "...", "t" : "-", "u" : "..-", "v" : "...-", "w" : ".--", "x" : "-..-", 
+               "y" : "-.--", "z" : "--.."}
+        out = []
+        for x in words:
+            X = list(x)
+            X = [dic.get(n, n) for n in X]
+            out.append("".join(X))
+        return len(set(out))
+        
