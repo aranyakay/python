4caaddb377a62e72a004117bc8d281aa3bef119e
JC <jjing.cc@gmail.com>
2019-07-29 13:57:08 +0800

    Update LC804.py

--- LC804.py
+++ LC804.py
@@ -1,10 +1,11 @@
-def lc804(self):
-    mosi = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..",
-    "--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
-    out = []
-    for x in self:
-        X = list(x)
-        for n,i in enumerate(X):
-            X[n] = mosi[ord(i)-97]
-        out.append("".join(X))
-    return(len(set(out)))
+class Solution:
+    def uniqueMorseRepresentations(self, words):
+        mosi = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..",
+            "--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        out = []
+        for x in words:
+            X = list(x)
+            for n,i in enumerate(X):
+                X[n] = mosi[ord(i)-97]
+            out.append("".join(X))
+        return len(set(out))
